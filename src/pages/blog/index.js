4bb952b8d952dbeff470f73fdeee83adf12bd208
rv <yo-maruya@rescala.jp>
2021-12-21 00:43:31 +0900

    add posts

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,38 +1,40 @@
-import * as React from "react";
+import React from 'react'
 
-import Layout from "../../components/Layout";
-import BlogRoll from "../../components/BlogRoll";
+import Layout from '../../components/Layout'
+import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
+         <div
+          className="center margin-top-2"
           style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
+            marginTop: '5em'
           }}
         >
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-              backgroundColor: "#f40",
-              color: "white",
-              padding: "1rem",
+              color: 'black',
             }}
           >
-            Latest Stories
+            Latest Blog Posts
           </h1>
         </div>
-        <section className="section">
+        <section className="section section--gradient">
           <div className="container">
-            <div className="content">
-              <BlogRoll />
+            <div className="section">
+              <div className="column is-12">
+              <div className="content">
+                <BlogRoll />
+              </div>
+              </div>
             </div>
+            
           </div>
         </section>
       </Layout>
-    );
+    )
   }
 }
